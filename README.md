c5174421b4e7ea4a4dcc328c83570c47313006e2
Mital <70700654+Mitali8620@users.noreply.github.com>
2024-05-16 00:31:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# advertisement_app
+
+A new Flutter project.
+
+## Getting Started
+
+## this app for two platform
+## Android
+## WEB
---
+++ android/app/src/main/kotlin/com/example/advertisement_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.advertisement_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
